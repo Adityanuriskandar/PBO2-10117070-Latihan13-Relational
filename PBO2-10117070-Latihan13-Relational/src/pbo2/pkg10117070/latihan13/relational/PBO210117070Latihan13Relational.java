8b25b0feb56d51b3db47228925abb32322e5167f
Aditya Nur Iskandar <43404691+Adityanuriskandar@users.noreply.github.com>
2018-09-23 12:51:17 +0700

    Add files via upload

---
+++ PBO2-10117070-Latihan13-Relational/src/pbo2/pkg10117070/latihan13/relational/PBO210117070Latihan13Relational.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117070.latihan13.relational;
+
+/**
+ *
+ * @author Aditya
+ * NAMA     : Aditya Nur Iskandar
+ * KELAS    : PBO2/IF2
+ * NIM      : 10117070
+ * Deskripsi Program : Program ini berisi program untuk mengetahui pengoprasian
+ * Relational
+ */
+public class PBO210117070Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bill = 10;
+        int bill2 = 20;
+        System.out.println("a == b = " +(bill == bill2));
+        System.out.println("a != b = " +(bill != bill2));
+        System.out.println("a > b = " +(bill > bill2));
+        System.out.println("a < b = " +(bill < bill2));
+        System.out.println("b >= a = " +(bill2 >= bill));
+        System.out.println("b <= a = " +(bill2 <= bill));
+ 
+    }
+    
+}
